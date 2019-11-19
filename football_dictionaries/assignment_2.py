540b5336ae5835d6afd39e64543c66e3ad2ef6c8
fatredbeaver <tedmundho@gmail.com>
2019-11-19 14:00:14 +0800

    Update assignment_2.py

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,8 @@
-def players_by_position(squads_list):
-    pass
+def players_by_positions(squads_list):
+    player_dict = players_as_dictionaries(squads_list)
+    position_dict = {}
+    for player in player_dict:
+        position = player['position']
+        position_dict.setdefault(position, [])
+        position_dict[position].append(player)
+    return position_dict
